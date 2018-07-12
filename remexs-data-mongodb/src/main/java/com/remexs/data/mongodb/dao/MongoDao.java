91080d9db08d3fed982fb3a0577f4eaa00fd0765
remexs <bigdrone@163.com>
2018-07-12 10:54:48 +0800

    新增路由功能

--- remexs-data-mongodb/src/main/java/com/remexs/data/mongodb/dao/MongoDao.java
+++
@@ -1,9 +0,0 @@
-package com.remexs.data.mongodb.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import org.springframework.data.repository.NoRepositoryBean;
-@NoRepositoryBean
-public interface MongoDao<T> extends MongoRepository<T, String> {
-
-}
